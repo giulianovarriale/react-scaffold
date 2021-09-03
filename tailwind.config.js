9b13f4d38ef5dad8b7fa14fbde06c9b48c9b12d9
Giuliano Varriale <giuliano.oca@gmail.com>
2021-09-03 11:21:49 +0200

    Add windmill

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,10 @@
-module.exports = {
+const windmill = require('@windmill/react-ui/config')
+
+module.exports = windmill({
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
   },
-  variants: {
-    extend: {},
-  },
+  variants: {},
   plugins: [],
-}
+})
